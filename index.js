ba4ee1beb418da63d6d569f323c2e7d93af0020c
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-11-24 01:06:13 +0200

    done

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+import { program } from "commander";
 import * as contactsService from "./contacts.js";
 
-const invokeAction = async ({ action, id }) => {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
       return console.log(allContacts);
-    case "getById":
+    case "get":
       const getContact = await contactsService.getContactById(id);
       return console.log(getContact);
-
+    case "add":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(newContact);
+    case "remove":
+      const deleteContactById = await contactsService.deleteContact(id);
+      return console.log(deleteContactById);
     default:
       console.log("Unknown action");
   }
 };
 
-// invokeAction({ action: "list" });
-invokeAction({ action: "getById", id: "rsKkOQUi80UsgVPCcLZZW" });
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+const options = program.opts();
+invokeAction(options);
